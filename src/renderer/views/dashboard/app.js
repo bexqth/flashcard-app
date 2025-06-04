14aed9add54cedeb975e1c6a9ba7f893c835ef16
bexqth <nicollsprite@gmail.com>
2025-06-04 19:37:07 +0200

    preparing create deck form

--- src/renderer/views/dashboard/app.js
+++ src/renderer/views/dashboard/app.js
@@ -1,4 +1,17 @@
-// dashboard.js
-function handleNewDeckClick() {
-    console.log("button clicked");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+function openModal() {
+    console.log("open modal");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
